05dfb5af959aae9404ebd456f9a4134f6763cdec
Dominik <dominik@pl.pl>
2019-02-01 19:56:24 +0100

    Init commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className={styles.mainPage}>
+            </div>
+        );
+    }
+}
