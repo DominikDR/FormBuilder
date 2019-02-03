8171c1909eaa5528ca22ec316f296024030433a4
Dominik <dominik@pl.pl>
2019-02-03 17:28:34 +0100

    Data are rendering in primitive way

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './App.css';
+import Form from '../Form/Form';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
     render() {
         return (
             <div className={styles.mainPage}>
+                <Form />
             </div>
         );
     }
