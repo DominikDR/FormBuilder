1f3a5ac153ac5702c6d1c53a39b3914fd630eeb3
Dominik <dominik@pl.pl>
2019-02-09 00:57:43 +0100

    Deleting elements works fine

--- src/components/FormList/FormList.js
+++ src/components/FormList/FormList.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { Fragment } from 'react';
 import styles from './FormList.css';
 import Form from './Form/Form';
@@ -19,46 +20,40 @@ export default class FormList extends React.Component {
                 },
                 [newForm.id]: newForm,
             },
-        }, () => console.log('TCL: FormList -> data state', this.state.data));
+        });
     }
 
     deleteSubForm = (clickedFormID) => {
-        
-        // deleteNodeChilds or deleteNestedChilds
-        const decreasedData = this.deleteNestedChilds(clickedFormID);
-		console.log('TCL: FormList -> deleteSubForm -> decreasedData', decreasedData)
-        const { parentID } = decreasedData[clickedFormID];
-        const parentSubForms = decreasedData[parentID].subForms;
-        decreasedData[parentID].subForms = decreasedData[parentID].subForms.filter(id => id !== clickedFormID);
-
-        delete decreasedData[clickedFormID];
-        this.setState({
-            data: {
-                ...decreasedData,
-            },
-        }, () => console.log('TCL: FormList -> data', this.state.data));
-    }
-
-    deleteNestedChilds = (nodeId) => {
-		console.log('TCL: FormList-> nodeId', nodeId)
         const { data } = this.state;
         const dataCopy = JSON.parse(JSON.stringify(data));
-		console.log('TCL: FormList -> deleteNestedChilds -> dataCopy', dataCopy)
-        dataCopy[nodeId].subForms.forEach((id) => {
-			console.log('TCL: FormList -> deleteNestedChilds -> subForm', id)
-            if (dataCopy[id].subForms) {
-                dataCopy[id].subForms.map(this.deleteNestedChilds);
-            }
-            delete dataCopy[id];
-        });
-        return dataCopy;
-    };
+        const sumNestedForms = this.sumNestedForms(clickedFormID);
+        console.log('sumNestedForms', sumNestedForms);
+        sumNestedForms.forEach((id) => { delete dataCopy[id]; });
+
+        const { parentID } = dataCopy[clickedFormID];
+        dataCopy[parentID].subForms = dataCopy[parentID].subForms.filter(id => id !== clickedFormID);
+
+        delete dataCopy[clickedFormID];
+        this.setState({
+            data: dataCopy,
+        }, () => { console.log("data state", this.state.data)});
+    }
+
+    sumNestedForms = (id) => {
+        const { data } = this.state;
+        const { subForms } = data[id];
+        return [...subForms, ...subForms.map(this.sumNestedForms).flat()];
+    }
 
     constructForm = (dataKey) => {
         const { data } = this.state;
         return (
             <Fragment key={dataKey}>
-                <Form formID={data[dataKey].id} addSubForm={this.addSubForm} deleteSubForm={this.deleteSubForm} />
+                <Form
+                    formID={data[dataKey].id}
+                    addSubForm={this.addSubForm}
+                    deleteSubForm={this.deleteSubForm}
+                />
                 <ol>
                     {data[dataKey].subForms.map(this.constructForm)}
                 </ol>
